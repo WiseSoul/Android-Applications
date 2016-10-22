a509fff6a96bf3bc7b9a9bde4defe42ae7140d07
WiseSoul <silviu.patras@gmail.com>
2016-10-23 02:20:22 +0300

    Add files via upload

---
+++ Workouts/main/java/com/example/workout/DetailActivity.java
@@ -0,0 +1,30 @@
+package com.example.workout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+
+    // This is a string used to save the Id of the Workout selected in the ListFragmentView.
+    public static final String EXTRA_WORKOUT_ID = "id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        // Create a workoutDetailFragment which will show the details of the Workout clicked in the ListFragmentView.
+        // We also need to specify the fragment's id defined the activity_detail.xml layout in order to show the details of the Workout.
+        // getFragmentManager() -> this is the manager of the fragment utilised in getting access to specific functions of the fragment.
+       WorkoutDetailFragment workoutDetailFragment = (WorkoutDetailFragment)
+               getFragmentManager().findFragmentById(R.id.detail_frag);
+        // Retrieve the Id of the Workout clicked in the ListFragmentView.
+        // This id was saved in the intent in the variable EXTRA_WORKOUT_ID.
+        int workoutId = (int) getIntent().getExtras().get(EXTRA_WORKOUT_ID);
+        // Set the workoutId in the workoutDetailFragment to show the details for the selected Workout.
+        workoutDetailFragment.setWorkout(workoutId);
+    }
+
+}
+
